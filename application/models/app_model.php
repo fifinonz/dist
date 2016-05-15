d710e9e4a19745955e8dee6d2b299d4fe9506876
fifinonz <muthoni.nguthi@betterworldsystems.co.ke>
2016-05-15 10:35:14 +0300

    first query

--- application/models/app_model.php
+++ application/models/app_model.php
@@ -1,4 +1,18 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class App_model extends CI_Model {
+
+	public function get_female_employees(){
+
+/*// Loading second db and running query.
+$CI = &get_instance();
+//setting the second parameter to TRUE (Boolean) the function will return the database object.
+$this->db2 = $CI->load->database('db2', TRUE);*/
+
+		$query = $this->db->query('SELECT * FROM `employment_logs`');
+
+		foreach ($query->result() as $key) {
+			echo $key->empid;
+		}
+	}
 }
